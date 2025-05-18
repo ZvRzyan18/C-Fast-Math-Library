8afb757ad6c6e4df1819ccef73b5c5a07583cd4e
ZvRzyan18 <rhenzalfred06@gmail.com>
2025-05-18 14:06:05 +0800

    add x86_64 clobber

--- src/cfm/round.c
+++ src/cfm/round.c
@@ -56,6 +56,7 @@ double cfm_round(double x) {
   "movss %%xmm0, %[output]"
   : [output]"=x"(x)
   : [input] "x"(x)
+  : "xmm0"
  );
  return x;
 }
@@ -67,6 +68,7 @@ float cfm_roundf(float x) {
   "movss %%xmm0, %[output]"
   : [output]"=m"(x)
   : [input] "m"(x)
+  : "xmm0"
  );
  return x;
 }
