a6c42dd48de193a0e9fea282c0a62202f8b6f767
ZvRzyan18 <rhenzalfred06@gmail.com>
2025-05-20 19:52:07 +0800

    organize fabs.c

--- src/cfm/fabs.c
+++ src/cfm/fabs.c
@@ -25,35 +25,42 @@
 #include "cfm/math.h"
 #include <stdint.h>
 
+#if defined(__aarch64__)
 
 double cfm_fabs(double x) {
-#if defined(__aarch64__)
  __asm__ volatile(
   "fmov x0, %d[result] \n"
   "and x0, x0, #0x7FFFFFFFFFFFFFFF \n"
   "fmov %d[result], x0 \n"
   : [result] "=w" (x)
   : [input] "w" (x)
+  : "x0"
  );
  return x;
-#else
-	uint64_t m = (*(uint64_t*)&x) & 0x7FFFFFFFFFFFFFFF;
-	return *(double*)&m;
-#endif
 }
 
 float cfm_fabsf(float x) {
-#if defined(__aarch64__)
  __asm__ volatile(
   "fmov w0, %s[result] \n"
   "and w0, w0, #0x7FFFFFFF \n"
   "fmov %s[result], w0 \n"
   : [result] "=w" (x)
   : [input] "w" (x)
+  : "w0"
  );
  return x;
-#else
-	uint32_t m = (*(uint32_t*)&x) & 0x7FFFFFFF;
-	return *(float*)&m;
-#endif
 }
+
+#else
+
+double cfm_fabs(double x) {
+ uint64_t m = (*(uint64_t*)&x) & 0x7FFFFFFFFFFFFFFF;
+ return *(double*)&m;
+}
+
+float cfm_fabsf(float x) {
+ uint32_t m = (*(uint32_t*)&x) & 0x7FFFFFFF;
+ return *(float*)&m;
+}
+
+#endif
