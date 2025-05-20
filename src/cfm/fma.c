9fb87ce142f05c453892cc4a2d5c84c5e15bc7bc
ZvRzyan18 <rhenzalfred06@gmail.com>
2025-05-20 12:16:19 +0800

    Aarch64 assembly support

--- src/cfm/fma.c
+++ src/cfm/fma.c
@@ -24,11 +24,36 @@
 
 #include "cfm/math.h"
 
+// TODO : implement cpu specific instruction
+#if defined(__aarch64__)
+
+__asm__ (
+ ".type cfm_fma, @function \n"
+
+ ".global cfm_fma \n"
+ "cfm_fma: \n"
+ "fmadd d0, d0, d1, d2 \n"
+ "ret \n"
+);
+
+__asm__ (
+ ".type cfm_fmaf, @function \n"
+
+ ".global cfm_fmaf \n"
+ "cfm_fmaf: \n"
+ "fmadd s0, s0, s1, s2 \n"
+ "ret \n"
+);
+
+#else 
+
 double cfm_fma(double x, double y, double z) {
-	return x * y + z;
+ return x * y + z;
 }
 
 float cfm_fmaf(float x, float y, float z) {
-	return x * y + z;
+ return x * y + z;
 }
+
+#endif
 
