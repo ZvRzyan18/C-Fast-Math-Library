0097be86390837d4b7ba02a09b2b669c8db10155
ZvRzyan18 <rhenzalfred06@gmail.com>
2025-05-20 12:58:49 +0800

    Add aarch64 clobber

--- src/cfm/fmod.c
+++ src/cfm/fmod.c
@@ -26,9 +26,9 @@
 #include <stdint.h>
 
 //TODO : implement using cpu specific instruction
+#if defined(__aarch64__)
 
 double cfm_fmod(double x, double y) {
-#if defined(__aarch64__)
  __asm__ volatile(
   "fdiv d3, %d[input1], %d[input2] \n"
   "frintz d3, d3 \n"
@@ -36,16 +36,13 @@ double cfm_fmod(double x, double y) {
   "fsub %d[result], %d[input1], d3 \n"
   : [result] "=w" (x)
   : [input1] "w" (x), [input2] "w" (y)
+  : "d3"
  );
  return x;
-#else
- return x - cfm_trunc(x / y) * y;
-#endif
 }
 
 
 float cfm_fmodf(float x, float y) {
-#if defined(__aarch64__)
  __asm__ volatile(
   "fdiv s3, %s[input1], %s[input2] \n"
   "frintz s3, s3 \n"
@@ -53,10 +50,23 @@ float cfm_fmodf(float x, float y) {
   "fsub %s[result], %s[input1], s3 \n"
   : [result] "=w" (x)
   : [input1] "w" (x), [input2] "w" (y)
+  : "s3"
  );
  return x;
-#else
- return x - cfm_truncf(x / y) * y;
-#endif
 }
+
+
+#else
+
+
+double cfm_fmod(double x, double y) {
+ return x - cfm_trunc(x / y) * y;
+}
+
+
+float cfm_fmodf(float x, float y) {
+ return x - cfm_truncf(x / y) * y;
+}
+
+#endif
 
