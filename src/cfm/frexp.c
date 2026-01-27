18945c42c06eb5e5a602e697ef15c915a93201ee
ZvRzyan18 <rhenzalfred06@gmail.com>
2026-01-27 17:04:44 +0800

    organize frexp.c code

--- src/cfm/frexp.c
+++ src/cfm/frexp.c
@@ -1,19 +1,24 @@
 #include "cfm/math.h"
+#include "cfm/float_bits.h"
 #include <stdint.h>
 
 //---------------DOUBLE------------------//
 
 double cfm_frexp(double x, int* _exp) {
-	*_exp = ((((*(int64_t*)&x) & 0x7FFFFFFFFFFFFFFF) >> 52)-1023) + 1;
-	uint64_t mantissa = 4607182418800017408U | ((*(uint64_t*)&x) & 0x000FFFFFFFFFFFFF);
-	return *(double*)&mantissa * 0.5;
+ double_bits bits, mantissa;
+ bits.f = x;
+ *_exp = (((int64_t)(bits.i & 0x7FFFFFFFFFFFFFFF) >> 52)-1023) + 1;
+ mantissa.i = 4607182418800017408U | (bits.i & 0x000FFFFFFFFFFFFF);
+ return mantissa.f * 0.5;
 }
 
 //---------------FLOAT------------------//
 
 float cfm_frexpf(float x, int* _exp) {
-	*_exp = ((((*(int32_t*)&x) & 0x7FFFFFFF) >> 23)-127) + 1;
-	uint32_t mantissa = 1065353216U | ((*(uint32_t*)&x) & 0x007FFFFF);
-	return *(float*)&mantissa * 0.5f;
+ float_bits bits, mantissa;
+ bits.f = x;
+ *_exp = (((int32_t)(bits.i & 0x7FFFFFFF) >> 23)-127) + 1;
+ mantissa.i = 1065353216U | ((*(uint32_t*)&x) & 0x007FFFFF);
+ return mantissa.f * 0.5f;
 }
 
